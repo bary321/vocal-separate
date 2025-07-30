384bf67e2298d080ecc280c1db0602b57d830737
ajy_4090_48g <>
2025-07-30 14:10:14 +0800

    更新.gitignore以排除新文件类型，更新requirements.txt以指定tensorflow和spleeter的版本，修改test.py中的文件路径和API地址，更新cfg.py中的web地址。

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 # cli 示例
-from spleeter.separator import Separator
-import os
-separator = Separator('spleeter:2stems', multiprocess=False)
-noextname="1"
-dirname=os.path.join("./static/files",noextname)
+# from spleeter.separator import Separator
+# import os
+# separator = Separator('spleeter:2stems', multiprocess=False)
+# noextname="1"
+# dirname=os.path.join("./static/files",noextname)
 
-separator.separate_to_file("./lao59.mp3", destination=dirname, filename_format="{instrument}.{codec}")
-a_name = f"{dirname}/{noextname}vocals.wav"
+# separator.separate_to_file("./lao59.mp3", destination=dirname, filename_format="{instrument}.{codec}")
+# a_name = f"{dirname}/{noextname}vocals.wav"
 
 # api 请求示例
 
 import requests
 # 请求地址
-url = "http://127.0.0.1:9999/api"
-files = {"file": open("C:\\Users\\c1\\Videos\\2.wav", "rb")}
+url = "http://172.31.0.7:9999/api"
+files = {"file": open("/root/src/baohuo_server/sucai/output/访谈_刘芳毅_audio.wav", "rb")}
 data={"model":"2stems"}
 response = requests.request("POST", url, timeout=600, data=data,files=files)
 print(response.json())
